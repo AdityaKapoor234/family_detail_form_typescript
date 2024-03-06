e95594ab37cb52305fd679bc50d10ffe1e7cd9db
divyanshchahar <divyanshchahar@outlook.com>
2024-03-06 17:34:24 +0530

    Added new types

--- src/types/ValidationConstrainTypes.ts
+++ src/types/ValidationConstrainTypes.ts
@@ -1,6 +1,9 @@
 type MaxLength = { value: number; message: string };
 
+type MinLength = { value: number; message: string };
+
 export type ValidationConstrainType = {
 	required?: boolean | string;
 	maxLength?: MaxLength;
+	minLength?: MinLength;
 };
