be703e962cd430f2ee817ec42f59a2090adfc18a
divyanshchahar <divyanshchahar@outlook.com>
2024-03-07 11:26:33 +0530

    added props

--- src/ui/components/TextInputComponent.tsx
+++ src/ui/components/TextInputComponent.tsx
@@ -1,21 +1,25 @@
 import { FieldValues, Path, UseFormRegister } from 'react-hook-form';
 import { ValidationConstrainType } from '../../types/ValidationConstrainTypes';
 
-export type PropTypes<T extends FieldValues> = {
+export type PropTypes<T extends FieldValues, V> = {
 	displayText: string;
 	inputName: Path<T>;
 	register: UseFormRegister<T>;
 	errorMessage?: string | undefined;
 	validationConstrain?: ValidationConstrainType;
+	onChangeHandler?: (arg: V) => void;
+	stateValue?: V;
 };
 
-function TextInputComponent<T extends FieldValues>({
+function TextInputComponent<T extends FieldValues, V>({
 	displayText,
 	inputName,
 	register,
 	errorMessage = undefined,
 	validationConstrain,
-}: PropTypes<T>) {
+	onChangeHandler = () => {},
+	stateValue = undefined,
+}: PropTypes<T, V>) {
 	return (
 		<div className="m-5">
 			<label className="form-label">{displayText}</label>
@@ -23,6 +27,13 @@ function TextInputComponent<T extends FieldValues>({
 				type="text"
 				className="form-control is-invalid"
 				{...register(inputName, { ...validationConstrain })}
+				onChange={(e) => {
+					if (stateValue) {
+						const newState = stateValue;
+						newState[inputName] = e.target.value;
+						onChangeHandler(newState);
+					}
+				}}
 			/>
 			<p className="invalid-feedback">{errorMessage}</p>
 		</div>
