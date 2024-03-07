86106dd02c2253892f755d82e7a62bdb95149685
divyanshchahar <divyanshchahar@outlook.com>
2024-03-07 17:58:33 +0530

    integrated routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 import './App.css';
-import ParentDataFormLayout from './ui/layouts/ParentDataFormLayout.js';
+import Routing from './routing/Routing.js';
 
 function App() {
-	return <ParentDataFormLayout />;
+	return <Routing />;
 }
 
 export default App;
